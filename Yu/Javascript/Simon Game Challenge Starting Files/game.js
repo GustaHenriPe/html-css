69465572d56c1ccf61b245661b3ba01682c6066c
Gustavo Henrique Pereira Junqueira <102169187+GustaHenriPe@users.noreply.github.com>
2023-06-20 13:34:32 -0300

    asdad

--- Yu/Javascript/Simon Game Challenge Starting Files/game.js
+++ Yu/Javascript/Simon Game Challenge Starting Files/game.js
@@ -2,34 +2,22 @@ var buttonColours = ["red", "blue", "green", "yellow"];
 var gamePattern = [];
 
 function nextSequence(){
+
     var randomNumber = Math.floor(Math.random() * 4);
-    return randomNumber;
+    var randomChosenColour = buttonColours[randomNumber];
+    gamePattern.push(randomChosenColour);
+
+    
+    $("#" + randomChosenColour).fadeIn(100).fadeOut(100).fadeIn(100);
+
+    
+    var audio = new Audio("sounds/" + randomChosenColour + ".mp3");
+    audio.play();
 }
 
-var randomChosenColour = buttonColours[nextSequence()]
-gamePattern.push(randomChosenColour)
+nextSequence()
 
-console.log(randomChosenColour)
-$("."+randomChosenColour).fadeOut(100).fadeIn(100)
 
-switch(randomChosenColour){
-        case "blue":
-            var blue = new Audio('sounds/blue.mp3')
-            blue.play();
-        break;
-        case "red":
-            var red = new Audio('sounds/red.mp3')
-            red.play();
-        break;
-        case "green":
-            var green = new Audio('sounds/green.mp3')
-            green.play();
-        break;
-        case "yellow":
-            var yellow = new Audio('sounds/yellow.mp3')
-            yellow.play();
-        break;
-    }
 
 
            
