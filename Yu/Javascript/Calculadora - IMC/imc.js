8eede26073d713ebc6bb59bec6827dae9b4458e9
Gustavo Henrique Pereira Junqueira <102169187+GustaHenriPe@users.noreply.github.com>
2023-07-07 13:27:17 -0300

    jjjjl

--- Yu/Javascript/Calculadora - IMC/imc.js
+++ Yu/Javascript/Calculadora - IMC/imc.js
@@ -1,10 +1,36 @@
+const calcular =  $("#calcular")
+
+
+calcular.on("click", calcularImc);
+
 function calcularImc(){
-    
+    const nome = $("#nome").val();
+    const altura = $("#altura").val();
+    const peso = $("#peso").val();
+    const resultado = $("#resultado")
+
+    if(nome !== "" && altura !== "" && peso !== ""){
+        const imc = Math.floor(peso/(altura*altura));
+        let classificacao ="";
+        if(imc < 18.5){
+            classificacao = "Abaixo do peso";
+        }else if(imc < 24.9){
+            classificacao = "com peso ideal";
+        }else if(imc < 29.9){
+            classificacao = "acima do peso";
+        }else if(imc < 39.9){
+            classificacao = "com Obesidade grau I";
+        }else if(imc >= 40){
+            classificacao = "com Obesidade grau II";
+        }else{
+            classificacao = "com Obesidade grau III";
+        }
+        resultado.text(`${nome} seu IMC é ${imc} e você está ${classificacao}`)
+    }else{
+       resultado.text("Preencha todos os campos!!!!!")
+    }
 }
-$("#calcular").on("click", function(event){
-    var nome = $("#nome").val()
-    var altura = parseFloat($("#altura").val())
-    var peso = parseFloat($("#peso").val())
-    var imc = peso/(Math.pow(altura));
-    alert(imc)
-});
+
+
+
+    
---
+++ Yu/Javascript/Lampada/lampada.js
@@ -0,0 +1,32 @@
+var quebrada = false
+
+$("img").dblclick(function(){
+    $("img").attr("src", "./img/quebrada.jpg");
+    quebrada = true
+})  
+/*$("#turnOn").click(function(){
+    if(quebrada == false){
+        $("img").attr("src", "./img/ligada.jpg");
+    }
+})
+
+$("#turnOff").click(function(){
+    if(quebrada == false){
+        $("img").attr("src", "./img/desligada.jpg");
+    }
+})
+
+$("img").mouseenter(function () { 
+    if(quebrada == false){
+        $("img").attr("src", "./img/ligada.jpg");
+    }
+});
+
+$("img").mouseleave(function () { 
+    if(quebrada == false){
+        $("img").attr("src", "./img/desligada.jpg");
+    }
+});*/ 
+
+
+
