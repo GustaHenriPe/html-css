9963cb745a6e8ed6ea7fba7544ddc49aa6a868d5
Gustavo Henrique Pereira Junqueira <102169187+GustaHenriPe@users.noreply.github.com>
2023-07-07 14:53:01 -0300

    ll

--- Yu/Javascript/Lampada/lampada.js
+++ Yu/Javascript/Lampada/lampada.js
@@ -1,8 +1,24 @@
 var quebrada = false
+var ligada = false
 
 $("img").dblclick(function(){
     $("img").attr("src", "./img/quebrada.jpg");
     quebrada = true
+})  
+
+$("#turnOn").click(function(){
+    if(quebrada == false){
+        if (ligada ){
+            $("img").attr("src", "./img/desligada.jpg");
+            $("#turnOn").text("LIGAR")
+            ligada = false
+        }else{
+            $("img").attr("src", "./img/ligada.jpg");
+            $("#turnOn").text("DESLIGAR")
+            ligada = true
+        }
+    }
+    
 })  
 /*$("#turnOn").click(function(){
     if(quebrada == false){
